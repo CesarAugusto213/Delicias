1defe7942f57d3bcf412f2c7eba666b35fc92510
Cesar <cesarsanchezcarrasco213@gmail.com>
2023-04-06 14:42:39 -0500

    Create Account Design Completed

--- app/src/main/java/com/cibertec/delicias/CreateAccountActivity.java
+++ app/src/main/java/com/cibertec/delicias/CreateAccountActivity.java
@@ -6,15 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create_account);
     }
 
-    public void goCreateAccount(View view){
-        startActivity(new Intent(this, CreateAccountActivity.class));
+    public void goLogin(View view){
+        startActivity(new Intent(this, LoginActivity.class));
     }
+
 }
