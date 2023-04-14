02d9d94c85f0884662018bed36243942e1e64af6
Cesar <cesarsanchezcarrasco213@gmail.com>
2023-04-13 22:48:28 -0500

    Add -> Mapa funcionando: Se cambio de emulador a un Pixel 4 API 30 con Android 11.0 R con las dependencias implementation 'com.google.android.gms:play-services-maps:18.1.0'y implementation 'com.google.android.gms:play-services-location:17.1.0'.

--- app/src/main/java/com/cibertec/delicias/MapActivity.java
+++ app/src/main/java/com/cibertec/delicias/MapActivity.java
@@ -1,14 +1,39 @@
 package com.cibertec.delicias;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public GoogleMap map;
+    public SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        map.setMyLocationEnabled(true);
     }
 }
